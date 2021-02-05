f51a3fc761856decc39119631338f345f5f27856
Ly Vireakdara <lyvireakdara@gmail.com>
2021-02-05 10:16:29 +0700

    last commit

---
+++ lib/services/networking.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+    print(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(response.body);
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
